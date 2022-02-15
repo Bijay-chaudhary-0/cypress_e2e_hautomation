a44ae33feeeafe104e5fb39236f2a083d34e0ce6
Bijay-chaudhary-0 <sam928310@gmail.com>
2022-02-15 21:51:30 +0545

    POST request fixed

--- cypress/integration/API_Test/POST.spec.js
+++ cypress/integration/API_Test/POST.spec.js
@@ -3,26 +3,42 @@ describe('post user request', () => {
     const name = faker.name.firstName()
     const email = faker.internet.email()
     const web = 'https://gorest.co.in'
-    const accessToken = 'ca3f5a91ab46e8484acfa6a3c678a162ac8edd08037a3a69d04f23493b54346f'
+    const accessToken = '756cc20c26596f0b0e30ed33cb5de8fe3278ca56d76ddd21e3095c844192a8a1'
+
     it('create user', () => {
         cy.request({
             method: 'POST',
-            url: web + '/public/v1/users',
+            url: web + '/public/v2/users',
             headers: {
                 'Authorization': 'Bearer ' + accessToken
             },
             body: {
-                "name": name,
-                "email": email,
+                "name": "Scarlet johanson",
+                "email": "Scar@let.com",
                 "status": "active",
                 "gender": "female"
             }
         })
             .then((ponse) => {
-                cy.log(JSON.stringify(ponse))
-                expect(ponse.body.data).has.property('name', name)
-                expect(ponse.body.data).has.property('email',email)
+                cy.log(JSON.stringify(ponse.body.id))
+                const ID = ponse.body.id
+                expect(ponse.body).has.property('name', "Scarlet")
+                expect(ponse.body).has.property('email',"Scar@let.com")
                 expect(ponse.status).to.eq(201)
+                cy.log(ponse.body.name)
+                cy.request({
+                    method: 'PUT',
+                    url: web + '/public-api/users/' + ID,
+                    headers: {
+                        'Authorization': 'Bearer ' + accessToken
+                    },
+                    body: {
+                        "name": 'Calcifer',
+                        "email": 'homie@email.com',
+                        "status": "inactive",
+                        "gender": "male"
+                    }
+                })
             })
     })
 })
